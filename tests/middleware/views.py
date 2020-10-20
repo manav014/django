14852dd952bbc2ea04d69e520aab4af73668be52
Carlton Gibson <carlton.gibson@noumenal.es>
2020-10-20 09:14:48 +0200

    Fixed #32124 -- Added per-view opt-out for APPEND_SLASH behavior.

--- tests/middleware/views.py
+++ tests/middleware/views.py
@@ -1,5 +1,25 @@
 from django.http import HttpResponse
+from django.utils.decorators import classonlymethod
+from django.views.generic import View
 
 
 def empty_view(request, *args, **kwargs):
     return HttpResponse()
+
+
+def sensitive_fbv(request, *args, **kwargs):
+    return HttpResponse()
+
+
+sensitive_fbv._should_append_slash = False
+
+
+class SensitiveCBV(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse()
+
+    @classonlymethod
+    def as_view(cls, **initkwargs):
+        view = super().as_view(**initkwargs)
+        view._should_append_slash = False
+        return view
